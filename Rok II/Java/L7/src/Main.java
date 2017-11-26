81766273158f6a322626748d67b6cfe058b02248
KrystianGrabowski <krystian987543@gmail.com>
2017-11-26 15:28:05 +0100

    </|\>

--- Rok II/Java/L7/src/Main.java
+++ Rok II/Java/L7/src/Main.java
@@ -2,8 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Okno o1 = new Okno();
 	}
 
 }
--- Rok II/Java/L7/src/Okno.java
+++ Rok II/Java/L7/src/Okno.java
@@ -1,4 +1,10 @@
+import java.awt.Frame;
 
-public class Okno {
-
+public class Okno extends Frame {
+	public Okno() {
+		super();
+		setSize(100, 100);
+		setVisible(true);
+		
+	}
 }
