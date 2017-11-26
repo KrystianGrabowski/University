57648f97fa86cfd59c981b1c39ccd5ccd8c5030d
KrystianGrabowski <krystian987543@gmail.com>
2017-11-26 15:19:24 +0100

    </|\>

---
+++ Rok II/Java/L7/src/Kreska.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class Kreska {
+	protected Point poczatek, koniec;
+	public final Color kolor;
+	
+	public Kreska (Point pocz, Point kon, Color kol) {
+		this.poczatek = pocz;
+		this.koniec = kon;
+		this.kolor = kol;
+	}
+}
