48aeed2fd33979aad5114694ae0b953c9793edfd
KrystianGrabowski <krystian987543@gmail.com>
2017-12-02 18:53:00 +0100

    </|\>

--- Rok II/Python/L8/PRL8Z2.py
+++ Rok II/Python/L8/PRL8Z2.py
@@ -10,46 +10,35 @@ class Music(Gtk.Window):
         self.grid.set_row_spacing(10)
         self.grid.set_column_spacing(10)
         self.add(self.grid)
-        button1 = Gtk.Button(label ="Button 1")
-        button2 = Gtk.Button(label="Button 2")
-        button3 = Gtk.Button(label="Button 3")
-        button4 = Gtk.Button(label="Button 4")
-        button5 = Gtk.Button(label="Button 5")
-        button6 = Gtk.Button(label="Button 6")
 
         scrolled = Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         #self.add(scrolled)
 
-        self.box = Gtk.Box(spacing = 5)
-        self.box.set_size_request(150,150)
-        self.box.pack_start(button3, True, True, 0)
-        button3.connect("clicked", self.load)
-        self.box.pack_start(button4, True, True, 0)
-        self.grid.add(self.box)
+        self.artists = Gtk.Box(spacing = 5)
+        self.artists.set_size_request(150,150)
+        self.grid.add(self.artists)
+        self.load_artists()
 
-        self.listbox = Gtk.ListBox()
-        self.listbox.add(button5)
-        self.listbox.add(button6)
-        self.grid.attach_next_to(self.listbox,self.box, Gtk.PositionType.BOTTOM,30,50)
-        #self.grid.attach(self.listbox,500,500,10,10)
+        self.albums = Gtk.ListBox()
+        self.grid.attach_next_to(self.albums, self.artists, Gtk.PositionType.BOTTOM,30,50)
 
-        self.listbox1 = Gtk.ListBox()
-        self.grid.attach_next_to(self.listbox1, self.listbox, Gtk.PositionType.RIGHT, 50,50)
+        self.tracks = Gtk.ListBox()
+        self.grid.attach_next_to(self.tracks, self.albums, Gtk.PositionType.RIGHT, 50,50)
 
         self.hb = Gtk.HeaderBar()
         self.hb.set_show_close_button(True)
-        self.hb.props.title = "records"
+        self.hb.props.title = "Records"
         self.set_titlebar(self.hb)
 
-    def load(self,widget):
+"""    def load(self,widget):
 
         self.button0 = Gtk.Label("buttonziro")
         self.listbox1 = Gtk.ListBox()
         self.grid.attach_next_to(self.listbox1, self.listbox, Gtk.PositionType.RIGHT, 50,50)
         self.listbox1.add(self.button0)
         self.show_all()
-        #self.listbox1.unselect_all()
+        #self.listbox1.unselect_all()"""
 
 
 window = Music()
