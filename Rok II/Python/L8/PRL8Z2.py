022359bb96e67012f61bba8430d18e2100c30d4a
KrystianGrabowski <krystian987543@gmail.com>
2017-12-03 19:25:50 +0100

    </|\>

--- Rok II/Python/L8/PRL8Z2.py
+++ Rok II/Python/L8/PRL8Z2.py
@@ -23,6 +23,11 @@ class Music(Gtk.Window):
         scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         #self.add(scrolled)
 
+        self.hb = Gtk.HeaderBar()
+        self.hb.set_show_close_button(True)
+        self.hb.props.title = "Records"
+        self.set_titlebar(self.hb)
+
         self.artists = Gtk.Box(spacing = 5)
         self.artists.set_size_request(150,150)
         self.grid.add(self.artists)
@@ -32,58 +37,52 @@ class Music(Gtk.Window):
         self.grid.attach_next_to(self.albums, self.artists, Gtk.PositionType.BOTTOM,30,50)
 
         self.tracks = Gtk.ListBox()
-        self.grid.attach_next_to(self.tracks, self.albums, Gtk.PositionType.RIGHT, 50,50)
+        self.grid.attach_next_to(self.tracks, self.albums, Gtk.PositionType.RIGHT, 30,40)
 
-        self.hb = Gtk.HeaderBar()
-        self.hb.set_show_close_button(True)
-        self.hb.props.title = "Records"
-        self.set_titlebar(self.hb)
+        self.albums = Gtk.ListBox()
+        self.grid.attach_next_to(self.albums, self.artists, Gtk.PositionType.BOTTOM,30,40)
+
+        self.menu = Gtk.ListBox()
+        self.grid.attach_next_to(self.menu, self.tracks, Gtk.PositionType.RIGHT, 30,40)
+
+        self.entry1 = Gtk.Entry()
+        self.entry2 = Gtk.Entry()
+        self.entry3 = Gtk.Entry()
+        self.menu.add(self.entry1)
+        self.menu.add(self.entry2)
+        self.menu.add(self.entry3)
+
+        buttonadd = Gtk.Button(label = "add")
+        buttonadd.connect("clicked", self.add_)
+        self.menu.add(buttonadd)
+
+    def add_(self, widget):
+        self.cur.execute('INSERT INTO track(tr, nazwa, art) VALUES(?, ?, ?)', ("Unforgiven", "Albumxyz","Mettalcia",))
+        self.c.commit()
+        self.cur.execute('SELECT * FROM artysta')
+        ex = self.cur.fetchall()
+        for i in ex:
+            print(i['nazwa'])
+        self.show_all()
+
+
+
+
+
+
+
 
     def load_artists(self):
         self.c = sqlite3.connect('base.db')
         self.c.row_factory = sqlite3.Row
         self.cur = self.c.cursor()
 
-        self.cur.execute("DROP TABLE IF EXISTS artysta")
-
-        self.cur.execute("""
-            CREATE TABLE artysta (
-                id TEXT PRIMARY KEY,
-                nazwa TEXT
-            )""")
-
-        self.cur.execute("""
-            DROP TABLE IF EXISTS album
-            """)
-        self.cur.execute("""
-            CREATE TABLE album (
-            id TEXT PRIMARY KEY,
-            nazwa TEXT ,
-            art TEXT,
-            FOREIGN KEY(art) REFERENCES artysta(nazwa)
-            )""")
-        self.cur.execute("""
-            DROP TABLE IF EXISTS track
-            """)
-        self.cur.execute("""
-            CREATE TABLE track (
-            id TEXT PRIMARY KEY,
-            tr TEXT,
-            nazwa TEXT,
-            art TEXT,
-            FOREIGN KEY(art) REFERENCES artysta(nazwa),
-            FOREIGN KEY(nazwa) REFERENCES album(nazwa)
-            )""")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS artysta(nazwa TEXT)" )
+        self.cur.execute("CREATE TABLE IF NOT EXISTS album(nazwa TEXT, art TEXT, FOREIGN KEY(art) REFERENCES artysta(nazwa))" )
+        self.cur.execute("CREATE TABLE IF NOT EXISTS track(tr TEXT, nazwa TEXT, art TEXT, FOREIGN KEY(art) REFERENCES artysta(nazwa), FOREIGN KEY(nazwa) REFERENCES album(nazwa))" )
 
         self.c.commit()
-        self.cur.execute('INSERT INTO artysta(nazwa) VALUES(?)', ("Slayer",))
-        self.cur.execute('INSERT INTO artysta(nazwa) VALUES(?)', ("Factory",))
-        self.cur.execute('INSERT INTO artysta(nazwa) VALUES(?)', ("Pantera",))
-        self.cur.execute('INSERT INTO album(nazwa, art) VALUES(?, ?)', ("Vulgar", "Pantera",))
-        self.cur.execute('INSERT INTO album(nazwa, art) VALUES(?, ?)', ("Hostile","Pantera",))
-        self.cur.execute('INSERT INTO album(nazwa, art) VALUES(?, ?)', ("Driven", "Pantera",))
-        self.cur.execute('INSERT INTO track(tr, nazwa, art) VALUES(?, ?, ?)', ("Walk", "Hostile", "Pantera",))
-        self.cur.execute('INSERT INTO track(tr, nazwa, art) VALUES(?, ?, ?)', ("Hollow", "Hostile","Pantera",))
+        #self.cur.execute('INSERT INTO artysta VALUES("SLAYER")')
         self.c.commit()
         self.cur.execute('SELECT * FROM artysta')
         artists_list = self.cur.fetchall()
@@ -120,7 +119,8 @@ class Music(Gtk.Window):
             self.tracks.add(l)
         self.show_all()
 
-
+    def newcd(self, widget):
+        self.show_all()
 
 
 window = Music()
