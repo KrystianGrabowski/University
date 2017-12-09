df354920d14d64d9e7079931924b8fb47956e664
KrystianGrabowski <krystian987543@gmail.com>
2017-12-09 15:00:54 +0100

    </|\>

--- Rok II/Java/L9/src/Calendar.java
+++ Rok II/Java/L9/src/Calendar.java
@@ -1,4 +1,36 @@
+import java.awt.GridLayout;
 
-public class Calendar {
+import javax.swing.*;
+
+public class Calendar extends JFrame {
+	
+	JTabbedPane panelMain = new JTabbedPane();
+	JPanel panelYear = new JPanel();
+	JPanel panelMonth = new JPanel();
+
+	
+	public Calendar() {
+		super("Calendar");
+		setSize(800,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo( null );
+		panels();
+		add(panelMain);
+		
+        setVisible(true);
+	}
+	
+	void panels() {
+		
+		panelYear.setLayout(new GridLayout(4, 3));
+		
+		panelYear.add(new JLabel("Label1"));
+		panelYear.add(new JLabel("Label2"));
+		
+		
+		panelMain.addTab("Rok", panelYear);
+		panelMain.addTab("Miesiac", panelMonth);
+		
+	}
 
 }
