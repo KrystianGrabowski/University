64eea7fc71db5affcc388379fa39d903ade141e1
KrystianGrabowski <krystian987543@gmail.com>
2017-12-11 13:09:30 +0100

    </|\>

--- Rok II/Java/L9/src/Calendar.java
+++ Rok II/Java/L9/src/Calendar.java
@@ -3,6 +3,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -14,6 +15,8 @@ public class Calendar extends JFrame {
 	public JPanel panelYear = new JPanel();
 	public MonthList panelMonth;
 	public JToolBar tb;
+	public JLabel year, month;
+	public JSpinner yearSp, monthSp;
 	private final String[] months = {"January", "February", "March", "April",
 									"May", "June", "July", "August",
 									"September","October", "November", "December"};
@@ -49,8 +52,55 @@ public class Calendar extends JFrame {
 	public void toolbr() {
 		tb = new JToolBar();
 		tb.setLayout(new FlowLayout());
-		JLabel j = new JLabel("ADSS");
-		tb.add(j);
+		year = new JLabel("Year:");
+		month = new JLabel("Month:");
+		tb.add(year);
+		SpinnerModel model_y = new SpinnerNumberModel( gc.get(GregorianCalendar.YEAR), 1582, null , 1);
+		yearSp = new JSpinner(model_y);
+		tb.add(yearSp);
+		
+		tb.add(month);
+		SpinnerModel model_m = new LoopSpinner (months, yearSp);	
+		monthSp = new JSpinner(model_m);
+		monthSp.setValue(months[gc.get(GregorianCalendar.MONTH)]);
+		tb.add(monthSp);
+
+
+	}
+	
+	public class LoopSpinner extends SpinnerListModel{
+		JSpinner spinner; 
+		
+		public LoopSpinner(String[] months, JSpinner spinner) {
+			super(months);
+			this.spinner = spinner;
+		}
+		
+		public Object getNextValue() {
+			List list = getList();
+            int index = list.indexOf(getValue());
+            if (index == list.size() - 1) {
+            	index = 0;
+            	
+            }
+            else {
+            	index++;
+            }
+            return list.get(index);
+		}
+		
+		public Object getPreviousValue() {
+			List list = getList();
+			int index = list.indexOf(getValue());
+			if (index == 0) {
+				index = list.size() - 1;
+			}
+			else {
+				index--;
+			}
+			return list.get(index);
+			
+		}
 	}
 
 }
