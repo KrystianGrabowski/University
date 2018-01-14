619e568350a9c186048d5a5bb87a2005451daec2
KrystianGrabowski <krystian987543@gmail.com>
2018-01-14 23:03:09 +0100

    </|\>

--- Rok II/Python/Projekt/PR_projekt.py
+++ Rok II/Python/Projekt/PR_projekt.py
@@ -69,20 +69,21 @@ class My_Gyms(Gtk.Window):
 
 
     def load_customers(self, widget):
-        self.customers_list = Gtk.ListBox()
-        self.grid.attach_next_to(self.customers_list, self.gyms_list,
-            Gtk.PositionType.RIGHT, 40, 50)
+
+        self.customers_list.gtk_container_remove( self.customers_list)
         self.cur.execute('SELECT * FROM customer WHERE name = ?', (widget.get_label(), ))
         customers_ = self.cur.fetchall()
         for customer_ in customers_:
-            b2 = Gtk.Button(label = customer_['fname'] + " " + customer_['sname'])
-            b2.connect("clicked", self.cust_info)
-            self.customers.append(b2)
+            print("DSADA")
+            b2 = Gtk.Label(customer_['fname'] + " " + customer_['sname'])
+            self.customers_list.add(b2)
 
-        for j in self.customers:
+        #self.customers_list.show_all()
+        '''for j in self.customers:
             self.customers_list.add(j)
-        self.show_all()
 
+        #self.show_all()
+        '''
     def cust_info(self, widget):
         print("info")
 
@@ -101,13 +102,12 @@ class My_Gyms(Gtk.Window):
 
 class Gymadd(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title = "Add gym")
-        self.set_default_size(300, 300)
+        Gtk.Window.__init__(self, title = "New Gym")
+        self.set_default_size(350, 350)
         self.grid = Gtk.Grid()
         self.grid.set_row_spacing(5)
         self.grid.set_column_spacing(3)
         self.add(self.grid)
-
 
         self.entry1 = Gtk.Entry()
         self.entry2 = Gtk.Entry()
@@ -115,19 +115,22 @@ class Gymadd(Gtk.Window):
 
         self.info_label = Gtk.Label("Podaj dane o siłowni: ")
         self.grid.attach(self.info_label, 5, 0, 40, 10)
-        label = Gtk.Label("Nazwa")
+        label = Gtk.Label("Name")
         self.grid.attach(label, 0, 8, 20, 10)
         self.grid.attach_next_to(self.entry1, label, Gtk.PositionType.RIGHT, 50, 10)
-        label2 = Gtk.Label("Ulica")
+        label2 = Gtk.Label("Street")
         self.grid.attach(label2, 0, 16, 20, 10)
         self.grid.attach_next_to(self.entry2, label2, Gtk.PositionType.RIGHT, 50, 10)
-        label3 = Gtk.Label("Numer")
+        label3 = Gtk.Label("Number")
         self.grid.attach(label3, 0, 24, 20, 10)
         self.grid.attach_next_to(self.entry3, label3, Gtk.PositionType.RIGHT, 50, 10)
 
-        add = Gtk.Button(label = "Add gym")
-        add.connect("clicked", self.add_m)
-        self.grid.attach_next_to(add, self.entry3, Gtk.PositionType.BOTTOM, 30,15)
+        self.add = Gtk.Button(label = "Add gym")
+        self.add.connect("clicked", self.add_m)
+        self.grid.attach_next_to(self.add, self.entry3, Gtk.PositionType.BOTTOM, 45, 16)
+
+        self.error_label = Gtk.Label()
+        self.grid.attach(self.error_label, 30, 50, 30, 10 )
 
     def add_m(self, widget):
         self.c = sqlite3.connect('gym.db')
@@ -137,29 +140,28 @@ class Gymadd(Gtk.Window):
         gym_name = self.entry1.get_text()
         gym_street = self.entry2.get_text()
         street_nr = self.entry3.get_text()
-
-        if self.entry3 == "" or self.entry1 == "" or self.entry2 == "":
-            self.info_label = Gtk.Label("Wprowadź wszystkie dane")
-        else :
+        if gym_name == "" or gym_street == "" or street_nr == "":
+            self.error_label.set_label("Brak danych")
+        else:
             self.cur.execute('INSERT INTO gym(name, street, nr) VALUES(?,?,?)', (gym_name, gym_street, street_nr))
+            self.error_label.set_label("Udało sie")
         self.c.commit()
+        self.show_all()
 
 class Custadd(Gtk.Window):
     def __init__(self, gyms):
         Gtk.Window.__init__(self, title = "Add customer")
 
-        self.set_default_size(300, 300)
+        self.set_default_size(350, 350)
         self.grid = Gtk.Grid()
         self.grid.set_row_spacing(5)
         self.grid.set_column_spacing(3)
         self.add(self.grid)
 
-
         self.entry1 = Gtk.Entry()
         self.entry2 = Gtk.Entry()
 
-        self.info_label = Gtk.Label("Podaj dane o kliencie: ")
-
+        self.info_label = Gtk.Label("Enter : ")
         self.grid.attach(self.info_label, 5, 0, 40, 10)
         label = Gtk.Label("Imię")
         self.grid.attach(label, 0, 8, 20, 10)
@@ -175,9 +177,12 @@ class Custadd(Gtk.Window):
             self.combobox.append_text(gym_name.get_label())
         self.combobox.connect("changed", self.combo_changed)
         self.grid.attach_next_to(self.combobox, label3, Gtk.PositionType.RIGHT, 50, 8)
-        add = Gtk.Button(label = "Add customer")
-        add.connect("clicked", self.add_m)
-        self.grid.attach_next_to(add, self.combobox, Gtk.PositionType.BOTTOM, 30,15)
+        self.add = Gtk.Button(label = "Add customer")
+        self.add.connect("clicked", self.add_m)
+        self.grid.attach_next_to(self.add, self.combobox, Gtk.PositionType.BOTTOM, 45, 16)
+
+        self.error_label = Gtk.Label()
+        self.grid.attach(self.error_label, 30, 50, 30, 10 )
 
     def add_m(self, widget):
         self.c = sqlite3.connect('gym.db')
@@ -187,15 +192,14 @@ class Custadd(Gtk.Window):
         cust_firstname = self.entry1.get_text()
         cust_surname = self.entry2.get_text()
         sel_gym = self.combobox.get_active_text()
-        print(sel_gym)
-
-        if sel_gym == None or self.entry1 == "" or self.entry2 == "":
-            self.info_label = Gtk.Label("Wprowadź wszystkie dane")
+        if sel_gym == None or cust_firstname == "" or cust_surname == "":
+            self.error_label.set_label("Brak danych")
         else:
             self.cur.execute('INSERT INTO customer(fname, sname, name) VALUES(?,?,?)', (cust_firstname, cust_surname, sel_gym))
+            self.error_label.set_label("Dodano")
 
         self.c.commit()
-
+        self.show_all()
 
     def combo_changed(self, widget):
         print("ewq")
