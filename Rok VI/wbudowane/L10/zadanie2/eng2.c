7f5c6c954ef14faa95646a6d9bafa4c90497ce91
KrystianGrabowski <krystian987543@gmail.com>
2020-01-06 19:07:21 +0100

    </|\>

--- Rok VI/wbudowane/L10/zadanie2/eng2.c
+++ Rok VI/wbudowane/L10/zadanie2/eng2.c
@@ -1,9 +1,13 @@
 #include <avr/io.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 #define BAUD 9600                          // baudrate
 #define UBRR_VALUE ((F_CPU)/16/(BAUD)-1)   // zgodnie ze wzorem
+
+uint16_t v;
 
 // inicjalizacja UART
 void uart_init()
@@ -42,11 +46,12 @@ void timer1_init()
   // ICR1  = 15624
   // częstotliwość 16e6/(256*(1+99)) = 625 Hz
   // wzór: datasheet 20.12.3 str. 164
-  ICR1 = 99;
-  TCCR1A = _BV(COM1A1) | _BV(WGM11);
-  TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS12);
+  ICR1 = 49;
+  TCCR1A = _BV(COM1A1);
+  TCCR1B = _BV(WGM13) | _BV(CS12);
   // ustaw pin OC1A (PB1) jako wyjście
   DDRB |= _BV(PB1);
+  TIMSK1 |= _BV(TOIE1) | _BV(ICIE1);
 }
 
 // inicjalizacja ADC
@@ -57,6 +62,26 @@ void adc_init()
   // częstotliwość zegara ADC 125 kHz (16 MHz / 128)
   ADCSRA  = _BV(ADPS0) | _BV(ADPS1) | _BV(ADPS2); // preskaler 128
   ADCSRA |= _BV(ADEN); // włącz ADC
+  ADCSRA |= _BV(ADIE); //Przerwania ADC
+}
+
+uint16_t data[16] = {3, 5, 7, 9, 11, 13, 15, 17, 20, 22, 24, 26, 28, 32, 35, 49};
+
+ISR(TIMER1_OVF_vect) {
+  ADCSRA |= _BV(ADSC);
+  TIFR1 |= _BV(TOV1);
+}
+
+ISR(TIMER1_CAPT_vect) {
+  ADCSRA |= _BV(ADSC);
+  TIFR1 |= _BV(ICF1);
+}
+
+ISR(ADC_vect) {
+  v = ADC;
+  OCR1A = data[v>>6];
+  ADCSRA |= _BV(ADIF);
+  printf("%"PRIu32"mV\r\n", (uint32_t) (1024 * 1100 / (v | 1)));
 }
 
 FILE uart_file;
@@ -73,21 +98,13 @@ int main()
   // uruchom licznik
   timer1_init();
   adc_init();
+  sei();
   // ustaw wypełnienie 50%
   OCR1A = ICR1/2;
-  uint16_t data[16] = {7, 5, 7, 9, 11, 13, 15, 17, 20, 22, 24, 26, 28, 32, 35, 69};
-
+  set_sleep_mode(SLEEP_MODE_IDLE);
 
   while(1) {
-    ADCSRA |= _BV(ADSC); // wykonaj konwersję
-    while (!(ADCSRA & _BV(ADIF))); // czekaj na wynik
-    ADCSRA |= _BV(ADIF); // wyczyść bit ADIF (pisząc 1!)
-    uint16_t v = ADC; // weź zmierzoną wartość (0..1023)
-    OCR1A = data[v>>6];
-    printf("Odczytano: %"PRIu16"\r\n",  v>>6);
+    sleep_mode();
   }
-
-
-  while(1);
 }
 
